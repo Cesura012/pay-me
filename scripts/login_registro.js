20ac2da83eef374e79746a9086d0741143ca9fa0
Damian-Zsiros-Prog <damjeronzalez23@gmail.com>
2024-12-01 22:20:08 -0500

    connect to api local: login/registro, transfers and add cards

--- scripts/login_registro.js
+++ scripts/login_registro.js
@@ -1,13 +1,74 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const API_URL = "http://localhost:3000";
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
 // Evento para abrir form de registro
-signUpButton.addEventListener('click', () =>
-  container.classList.add('right-panel-active')
+signUpButton.addEventListener("click", () =>
+  container.classList.add("right-panel-active")
 );
 
 // Evento para regresar al form de iniciar sesión
-signInButton.addEventListener('click', () =>
-  container.classList.remove('right-panel-active')
+signInButton.addEventListener("click", () =>
+  container.classList.remove("right-panel-active")
 );
+
+const formSignIn = document.getElementById("formSignIn");
+
+formSignIn.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  console.log();
+  const email = document.getElementById("emailUser").value;
+  const password = document.getElementById("passwordUser").value;
+
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await res.json();
+  if (data.error && data.error.length > 0) {
+    alert(data.message);
+  } else {
+    localStorage.setItem(
+      "infoUser",
+      JSON.stringify({
+        email,
+        password,
+      })
+    );
+    window.location.href = `./dashboard.html`;
+  }
+});
+
+const formSignUp = document.getElementById("formSignUp");
+
+formSignUp.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const name = document.getElementById("nombre").value;
+  const apellido = document.getElementById("apellido").value;
+  const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value;
+
+  const res = await fetch(`${API_URL}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      first_name: name,
+      last_name: apellido,
+      email,
+      password,
+    }),
+  });
+  const data = await res.json();
+  if (data.error && data.error.length > 0) {
+    alert(`${data.message}: ${data.error}`);
+  } else {
+    alert("Usuario registrado correctamente!");
+    window.location.href = `./login_registro.html`;
+  }
+});
--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,51 +1,110 @@
 //Mostrar menú en todas las pantallas
 function loadSidebar() {
-    fetch('sidebar.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error al cargar la barra lateral');
-            }
-            return response.text();
-        })
-        .then(html => {
-            document.querySelector('.sidebar-container').innerHTML = html;
+  fetch("sidebar.html")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error al cargar la barra lateral");
+      }
+      return response.text();
+    })
+    .then((html) => {
+      document.querySelector(".sidebar-container").innerHTML = html;
 
-            initializeSidebarNavigation();
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+      initializeSidebarNavigation();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 //Funcion navegación entre paneles
 function initializeSidebarNavigation() {
-    const currentPath = window.location.pathname.split('/').pop();
-    const buttons = document.querySelectorAll('.btns-menu button');
+  const currentPath = window.location.pathname.split("/").pop();
+  const buttons = document.querySelectorAll(".btns-menu button");
 
-    buttons.forEach(button => {
-        const link = button.getAttribute('data-link');
-        
-        if (link === currentPath) {
-            button.classList.add('active');
-        }
+  buttons.forEach((button) => {
+    const link = button.getAttribute("data-link");
 
-        button.addEventListener('click', () => {
-            navigateTo(link);
-        });
-    });
-
-    //Redirigiar al Dashboard al hacer clic en el logo
-    const logo = document.querySelector('.sidebar h1');
-    if (logo) {
-        const dashboardLink = logo.getAttribute('data-link');
-        logo.addEventListener('click', () => {
-            navigateTo(dashboardLink);
-        });
+    if (link === currentPath) {
+      button.classList.add("active");
     }
+
+    button.addEventListener("click", () => {
+      navigateTo(link);
+    });
+  });
+
+  //Redirigiar al Dashboard al hacer clic en el logo
+  const logo = document.querySelector(".sidebar h1");
+  if (logo) {
+    const dashboardLink = logo.getAttribute("data-link");
+    logo.addEventListener("click", () => {
+      navigateTo(dashboardLink);
+    });
+  }
 }
 
 function navigateTo(url) {
-    window.location.href = url;
+  window.location.href = url;
+}
+const infoUser = JSON.parse(localStorage.getItem("infoUser"));
+const API_URL = "http://localhost:3000";
+
+const formMakeTransfer = document.getElementById("formMakeTransfer");
+if (formMakeTransfer) {
+  formMakeTransfer.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const recipientEmail = document.getElementById("recipientEmail").value;
+    const amount = document.getElementById("amount").value;
+    const message = document.getElementById("message").value;
+    const res = await fetch(`${API_URL}/transfers`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        sender_id: "1",
+        recipient_email: recipientEmail,
+        amount,
+        message,
+      }),
+    });
+    const data = await res.json();
+    if (data.error && data.error.length > 0) {
+      alert(`${data.message}: ${data.error}`);
+    } else {
+      alert(data.message);
+    }
+  });
+}
+const formAddCard = document.getElementById("formAddCard");
+
+if (formAddCard) {
+  formAddCard.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const cardNumber = document.getElementById("cardNumber").value;
+    const expirationDate = document.getElementById("expirationDate").value;
+    const cvv = document.getElementById("cvv").value;
+    const res = await fetch(`${API_URL}/addCards`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user_id,
+        card_number: cardNumber,
+        card_holder: "Jhon Doe",
+        expiration_date: expirationDate,
+        card_type: "debit",
+      }),
+    });
+    const data = await res.json();
+    if (data.error && data.error.length > 0) {
+      alert(`${data.message}: ${data.error}`);
+    } else {
+      alert(data.message);
+    }
+  });
 }
 
-document.addEventListener('DOMContentLoaded', loadSidebar);
+document.addEventListener("DOMContentLoaded", loadSidebar);
